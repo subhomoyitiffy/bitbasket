7fb18da4d64f6c5af3b3bbacfa2705e4d84eeb2d
chayansamanta <work.chayan2020@gmail.com>
2025-01-19 13:28:35 +0530

    Login success test

--- tests/Feature/Api/ApiLoginTest.php
+++ tests/Feature/Api/ApiLoginTest.php
@@ -7,8 +7,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 
-use App\Models\User;
 use Tests\TestCase;
+use App\Models\User;
 
 class ApiLoginTest extends TestCase
 {
@@ -22,6 +22,7 @@ class ApiLoginTest extends TestCase
         $user = User::factory()->create([
             'role_id'=> env('MEMBER_ROLE_ID'),
             'password' => Hash::make('password123'),
+            'status'=> 1
         ]);
 
         // Simulate a successful login
