c4c861c6c83f0e11dd8ee6ab09c88e206aeaad57
chayansamanta <work.chayan2020@gmail.com>
2025-01-31 23:03:33 +0530

    add membership related function

--- app/Http/Controllers/Api/CommonController.php
+++ app/Http/Controllers/Api/CommonController.php
@@ -5,15 +5,25 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Api\BaseApiController as BaseApiController;
 use Illuminate\Http\Request;
 use App\Models\State;
+use App\Models\Package;
 
 class CommonController extends BaseApiController
 {
     /**
-     * Return all UAT state list @JSON.
+     * Return all state list @JSON.
     */
     public function getStates()
     {
-            $status = State::select('id', 'name')->get();
-            return $this->sendResponse($status, 'UAE State list');
+            $states = State::select('id', 'name')->get();
+            return $this->sendResponse($states, 'UAE State list');
+    }
+
+    /**
+     * Return all active packages list @JSON.
+    */
+    public function getPackages($status = 1)
+    {
+            $packages = Package::where('status', $status)->get();
+            return $this->sendResponse($packages, 'All active packages/subscriptions list');
     }
 }
