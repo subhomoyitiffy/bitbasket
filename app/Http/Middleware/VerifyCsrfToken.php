110517af3d3d64dbd5e2a6a9e07d00deca7d88b5
subhomoy samanta <subhomoy@keylines.net>
2025-03-27 07:18:21 +0530

    csrf token disabled for admin login

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,6 +14,6 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         'api/*', // routes group
         'staffapi/*', // routes group
-        'login'
+        'admin/login'
     ];
 }
