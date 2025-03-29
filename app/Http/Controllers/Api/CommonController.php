6817c41ceb2fad75631d3db0750b29b3e3308d61
chayansamanta <work.chayan2020@gmail.com>
2025-03-29 21:54:33 +0530

    Add FAQ list for frontend

--- app/Http/Controllers/Api/CommonController.php
+++ app/Http/Controllers/Api/CommonController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Api\BaseApiController as BaseApiController;
 use Illuminate\Http\Request;
 use App\Models\State;
 use App\Models\Package;
+use App\Models\Faq;
+use App\Models\FaqCategory;
 
 class CommonController extends BaseApiController
 {
@@ -25,5 +27,18 @@ class CommonController extends BaseApiController
     {
             $packages = Package::where('status', $status)->get();
             return $this->sendResponse($packages, 'All active packages/subscriptions list');
+    }
+
+    /**
+     * Return all active FAQ & FAQ category list @JSON.
+    */
+    public function getFaqs()
+    {
+        $faqs = Faq::where('status', 1)->get();
+        $faq_categorys = FaqCategory::where('status', 1)->get();
+        return $this->sendResponse([
+            'faqs'=> $faqs,
+            'faq_categorys'=> $faq_categorys,
+        ], 'All active FAQ & FAQ category list');
     }
 }
